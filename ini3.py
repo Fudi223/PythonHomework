139a015a20f7e2994ffe58bd003e544f3a67922a
Fudi223 <fdudi@hotmail.com>
2024-11-14 17:32:38 +0100

    "Added solution to INI3"

---
+++ ini3.py
@@ -0,0 +1,7 @@
+text ="8cuPpCasarca4QGuciSqAOkU85uja2b3vmM8XMrrfalcinellus1yR89CEjARkz0MK4QGSya839Xe5CEton93yvdDixUgsIq1Ssliv6FY9zxNBLNAhlUxmswHwpwg1ev0qYzA8PRLJExXzwpXAJQlJsS2hf87Y6YGLY0xrGi9n."
+a = 5
+b = 11
+c = 40
+d = 50
+
+print(text[a:b]+" "+ text[c:d])
