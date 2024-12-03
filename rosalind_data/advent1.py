68b26f6e73eaffee2063c122748d7724fa8e65d5
Fudi223 <fdudi@hotmail.com>
2024-12-03 14:36:24 +0100

    "advent 2 attempts"

--- rosalind_data/advent1.py
+++ rosalind_data/advent1.py
@@ -16,3 +16,4 @@ list_difference = [abs(a - b) for a,b in zip(list_a_sorted, list_b_sorted)]
 sum_differences = sum(list_difference)
 
 print(sum_differences)
+
--- rosalind_data/util.py
+++ rosalind_data/util.py
@@ -3,9 +3,18 @@
 # a library of utility functions for Rosalind exercises
 
 def read_input(filepath):
-with open(filepath, 'r') as infile:
-    lines = infile.readlines()
-    stripped = []
-    for line in lines:
-        stripped.append(line.strip())
-return stripped
+    with open(filepath, 'r') as infile:
+        lines = infile.readlines()
+        stripped = []
+        for line in lines:
+            stripped.append(line.strip())
+    return stripped
+
+
+# Tims file import os:
+
+import os # Used for global same directory call to "safeornot.txt"
+
+# Global call to safeornot.txt
+here = os.path.dirname(os.path.abspath(__file__))
+safeornot = os.path.join(here, "FILENAME")
