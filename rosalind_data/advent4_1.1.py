15373737b449af6c28f9a80154d989c8ada1c1fd
Fudi223 <fdudi@hotmail.com>
2024-12-06 23:10:20 +0100

    "fixed advent 1.1"

--- rosalind_data/advent4_1.1.py
+++ rosalind_data/advent4_1.1.py
@@ -8,23 +8,6 @@ xmas_crossword = read_input("C:/PythonHomework/rosalind_data/advent_text.txt")
 
 expected_string = "XMAS"
 
-xmas_crossword = [
-    ["M", "M", "M", "S", "X", "X", "M", "A", "S", "M"],
-    ["M", "S", "A", "M", "X", "M", "S", "M", "S", "A"],
-    ["A", "M", "X", "S", "X", "M", "A", "A", "M", "M"],
-    ["M", "S", "A", "M", "A", "S", "M", "S", "M", "X"],
-    ["X", "M", "A", "S", "A", "M", "X", "A", "M", "M"],
-    ["X", "X", "A", "M", "M", "X", "X", "A", "M", "A"],
-    ["S", "M", "S", "M", "S", "A", "S", "X", "S", "S"],
-    ["S", "A", "X", "A", "M", "A", "S", "A", "A", "A"],
-    ["M", "A", "M", "M", "M", "X", "M", "M", "M", "M"],
-    ["M", "X", "M", "X", "A", "X", "M", "A", "S", "X"],
-]
-
-
-expected_string = "XMAS"
-
-
 def check_horizontal(y, x):
     number_matched = 0
 
