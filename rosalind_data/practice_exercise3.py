ce70882c6fc82e62c3a77a87afc9e4456b0addf4
Fudi223 <fdudi@hotmail.com>
2024-12-09 09:46:10 +0100

    "deleted file"

--- rosalind_data/practice_exercise3.py
+++ rosalind_data/practice_exercise3.py
@@ -1,17 +1,6 @@
-#!/usr/bin/env
+```
 
-# 3. Check if All Numbers in a Sublist are Even
+file = open("C:/Users/fdudi/filefile.txt","r")
 
-# Task:
-# Given a list of lists, check if all numbers in any sublist are even.
-# Return True if all numbers in the sublist are even, otherwise return False.
-
-number_list = [[1,2,4,5,5,5],[4,4,2,2,3,1],[2,76,12,52,64],[23,15,643,1,123],[123,123,6,6,2,2,3,4],[1,6,87,3,5,1,23,5,6]]
-
-
-def even(number_list):
-    for sublist in number_list:
-        all_is_even = (numbers % 2 == 0 for numbers in sublist):
-
-            
-print(even(number_list))
+print(file.read())
+```
