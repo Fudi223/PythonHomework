bb1e7ce0797c13d0a21e4b3f5f2bbf75a93f7ebc
Fudi223 <fdudi@hotmail.com>
2024-12-10 11:04:45 +0100

    "fudidle update"

--- rosalind_data/fudidle.py
+++ rosalind_data/fudidle.py
@@ -1,21 +1,25 @@
 
 # the word that is searched for:
 victory_word = "SCARF"
-
-# input prompt: 
-print("GUESS THE 5 LETTER WORD! If a letter is the correct one at the correct position, you will get a 'O' sign! If you have the right letter but it is at the wrong position, you will get a '/' sign! If a letter is wrong and at the wrong position, you will get a 'X' sign. Good luck!")
-word_input = input("Which word do you want to try? : ")
-
-while word_input != victory_word:
-    print("Try again!")
+while True:
+    # input prompt: 
+    # print("GUESS THE 5 LETTER WORD! If a letter is the correct one at the correct position, you will get a 'O' sign! If you have the right letter but it is at the wrong position, you will get a '/' sign! If a letter is wrong and at the wrong position, you will get a 'X' sign. Good luck!")
     word_input = input("Which word do you want to try? : ")
+
+    # letter_grid = "O_O_O_O_O"
+
+    # check if a letter from the correct word is present and returns whether the position is correct where its standing, or not.
+    wlwp_grid = ""
+    # wlwp = Wrong Letter Wrong Place
+    # go over every letter in the input word
+    for w_letter, v_letter in zip(victory_word, word_input):
+        # compare every letter from the input word to the victory word
+        if w_letter == v_letter:
+            wlwp_grid += "O"
+        else:
+            if w_letter != v_letter:
+                wlwp_grid += "X"
+    print(wlwp_grid)
     
-print("Congratulations!")
-# if victory_word == word_input:
-#   print("O O O O O")
-#   print("Congratulations!")
-
-# if victory_word != word_input:
-#   print("X X X X X")
-#   print("Try again!")
-
+    if word_input == victory_word:
+        break
