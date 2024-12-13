c0440917ea05206abb0229d84c7ebb884fb56bae
Fudi223 <fdudi@hotmail.com>
2024-12-13 10:09:33 +0100

    "util update"

--- rosalind_data/util.py
+++ rosalind_data/util.py
@@ -18,3 +18,16 @@ def read_input(filepath):
 # # Global call to safeornot.txt
 # here = os.path.dirname(os.path.abspath(__file__))
 # safeornot = os.path.join(here, "FILENAME")
+def strand_complementing(file):    
+    sample_reversed = file[::-1]
+    sample_complemented = ""
+    for base in sample_reversed:
+        if base == "A":
+            sample_complemented += "T"
+        if base == "T":
+            sample_complemented += "A"
+        if base == "G":
+            sample_complemented += "C"
+        if base == "C":
+            sample_complemented += "G"
+    return sample_complemented
